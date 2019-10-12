0f8a11d468b8d06b744c3cd694f3a20487e7b8bc
sheodox <imagamer27@msn.com>
2019-10-12 12:40:51 -0500

    added missing files from last commit, changes for a proper build pack all through webpack, and adding scss builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,54 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
     mode: "development",
-    entry: "./src/renderer.tsx",
+    entry: "./src/electron.ts",
+    devtool: "source-map",
+    target: 'electron-main',
 
     output: {
-        filename: 'bundle.js',
+        filename: 'electron.js',
         path: path.resolve(__dirname, 'dist')
     },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js"]
     },
 
     module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    }
-                ]
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
+        rules: [{
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: [{ loader: "ts-loader" }]
+        }, {
+            enforce: "pre",
+            test: /\.js$/,
+            loader: "source-map-loader"
+        }
         ]
     }
-};
+}, {
+    mode: 'development',
+    entry: './src/renderer.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            include: /src/,
+            use: [{ loader: 'ts-loader' }]
+        }, {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'renderer.js'
+    }
+}
+];
