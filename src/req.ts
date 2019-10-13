efec3ed294a1ad90fadb6ebce059f59acff56cea
sheodox <imagamer27@msn.com>
2019-10-13 14:56:55 -0500

    add a shorter timeout so the application doesn't appear to freeze for a couple minutes

--- src/req.ts
+++ src/req.ts
@@ -1,7 +1,23 @@
 import https from 'https';
 
+const REQUEST_TIMEOUT_MS = 3000;
+
 export const get = (url: string) => {
-    return new Promise((resolve: Function, reject: Function) => {
+    return new Promise((resolve: Function, _reject: Function) => {
+        let timedout = false;
+        const timeout = setTimeout(() => {
+            timedout = true;
+            _reject(new Error(`Timed out on ${url}`));
+        }, REQUEST_TIMEOUT_MS);
+
+        const reject = (error: Error) => {
+            if (timedout) {
+                return;
+            }
+            clearTimeout(timeout);
+            _reject(error);
+        }
+
         https.get(url, {
             timeout: 1000
         },
@@ -19,7 +35,10 @@ export const get = (url: string) => {
                         return;
                     }
                     try {
-                        resolve(JSON.parse(rawData));
+                        if (!timedout) {
+                            resolve(JSON.parse(rawData));
+                            clearTimeout(timeout);
+                        }
                     } catch (e) {
                         console.error(`Error parsing data for ${url}`)
                         reject(e);
